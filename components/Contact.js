4555c6434f39a8bad450b708bdd48f3d481ed27b
travaScriptES7 <32422093+travaScriptES7@users.noreply.github.com>
2021-01-07 22:03:33 -0800

    netlify attempt

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <div className='container' style={{ marginTop: '25px', textAlign: 'center' }}>
       <h1>Contact</h1>
-      <form className="contact-form" action="/success" name="contact" method="POST" netlify >
+      <form onClick={handleSubmit} className="contact-form" action="/success" name="contact" method="POST" netlify >
         <input type="hidden" name="form-name" value="contact" />
         <div className="form-group">
           <label htmlFor="yourname" >Name</label>
