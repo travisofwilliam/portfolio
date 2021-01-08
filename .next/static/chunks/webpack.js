6638faf61aac41e9a6b9fc789e19653700fa7e37
travaScriptES7 <32422093+travaScriptES7@users.noreply.github.com>
2021-01-07 19:51:53 -0800

    added emailjs to contact form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cde4e0bdce94e4e784ca";
+/******/ 	var hotCurrentHash = "71e2f816950836537369";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/contact.js
+++ pages/contact.js
@@ -1,21 +1,34 @@
+import emailjs, { send } from 'emailjs-com'
+
 const Placeholder = () => {
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_eyf7ofu', 'template_8293uds', e.target, 'user_hguX7dVSTsDnOIyZs1nd9')
+      .then((result) => {
+        console.log(result.text)
+      }, (error) => {
+        console.log(error.text)
+      })
+  }
   return (
     <div className='container' style={{ marginTop: '25px', textAlign: 'center' }}>
       <h1>Contact</h1>
-      <form>
+      <form className="contact-form" onSubmit={sendEmail}>
         <div className="form-group">
           <label htmlFor="formControlInput1">Name</label>
-          <input type="text" className="form-control" id="formControlInput1" placeholder="Name" />
+          <input type="text" className="form-control" placeholder="Name" name="user_name" />
         </div>
         <div className="form-group">
           <label htmlFor="formControlInput2">Email address</label>
-          <input type="email" className="form-control" id="formControlInput1" placeholder="name@example.com" />
+          <input type="email" className="form-control" placeholder="name@example.com" name="user_email" />
         </div>
         <div className="form-group">
           <label htmlFor="exampleFormControlTextarea1">Message</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <textarea className="form-control" name="message" rows="3"></textarea>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" value="send" className="btn btn-primary">Submit</button>
       </form>
     </div>
   )
